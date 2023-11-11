3550a1738461fb51afef20904931ab99a0400c24
EmilK102 <vip.elick@gmail.com>
2023-11-11 15:50:46 +0500

    add docker

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@localhost/genesiswarehouse"
+SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@postgres/genesiswarehouse"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
